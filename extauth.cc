5fa4ece01e6c80036aabdcb2f0d69aad7175b465
Pastèque <519134691@qq.com>
2020-03-27 19:32:59 -0400

    Exp no2

--- extauth.cc
+++ extauth.cc
@@ -16,7 +16,6 @@ ExtAuth::~ExtAuth() { ASSERT(!auth_request_); }
 
 FilterHeadersStatus ExtAuth::decodeHeaders(HeaderMap& headers, bool) {
   log().info("ExtAuth Request received; contacting auth servers");
-  return FilterHeadersStatus::StopIteration;
 
   // Copy original headers as a JSON object
   std::string json("{");
